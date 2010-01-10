899d08c3a09234961e16f6f100d88b659a7e8347
danchi.cheng <danchi.cheng@33327d0a-b64f-11de-97ce-e783fd196cab>
2010-01-10 12:50:55 +0000

    clients xml in different folders view peer list in tracker automatic local hosting ip address OpenfileDialog to select path

--- cevfyp/TrackerServer/TrackerServer/TrackerMainFrm.Designer.cs
+++ cevfyp/TrackerServer/TrackerServer/TrackerMainFrm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TrackerServer
 {
-    partial class Form1
+    partial class TrackerMainFrm
     {
         /// <summary>
         /// Required designer variable.
@@ -37,51 +37,50 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.preferenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.btnReset = new System.Windows.Forms.Button();
+            this.TreelistView = new System.Windows.Forms.ListView();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbClientlist
             // 
-            this.rtbClientlist.Location = new System.Drawing.Point(12, 58);
+            this.rtbClientlist.Location = new System.Drawing.Point(12, 63);
             this.rtbClientlist.Name = "rtbClientlist";
-            this.rtbClientlist.Size = new System.Drawing.Size(268, 166);
+            this.rtbClientlist.Size = new System.Drawing.Size(220, 180);
             this.rtbClientlist.TabIndex = 0;
             this.rtbClientlist.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 43);
+            this.label1.Location = new System.Drawing.Point(8, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.Size = new System.Drawing.Size(66, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Online Client";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 29);
+            this.label2.Location = new System.Drawing.Point(235, 47);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 12);
+            this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tree";
             // 
             // cbbTree
             // 
             this.cbbTree.FormattingEnabled = true;
-            this.cbbTree.Items.AddRange(new object[] {
-            "T1",
-            "T2"});
-            this.cbbTree.Location = new System.Drawing.Point(42, 21);
+            this.cbbTree.Location = new System.Drawing.Point(270, 39);
             this.cbbTree.Name = "cbbTree";
-            this.cbbTree.Size = new System.Drawing.Size(77, 20);
+            this.cbbTree.Size = new System.Drawing.Size(77, 21);
             this.cbbTree.TabIndex = 3;
+            this.cbbTree.SelectedIndexChanged += new System.EventHandler(this.cbbTree_SelectedIndexChanged);
             // 
             // btnOn
             // 
-            this.btnOn.Location = new System.Drawing.Point(12, 230);
+            this.btnOn.Location = new System.Drawing.Point(12, 249);
             this.btnOn.Name = "btnOn";
-            this.btnOn.Size = new System.Drawing.Size(75, 21);
+            this.btnOn.Size = new System.Drawing.Size(75, 23);
             this.btnOn.TabIndex = 4;
             this.btnOn.Text = "On";
             this.btnOn.UseVisualStyleBackColor = true;
@@ -92,34 +91,44 @@
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.preferenceToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(120, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(131, 26);
             // 
             // preferenceToolStripMenuItem
             // 
             this.preferenceToolStripMenuItem.Name = "preferenceToolStripMenuItem";
-            this.preferenceToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.preferenceToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.preferenceToolStripMenuItem.Text = "Preference";
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(94, 230);
+            this.btnReset.Enabled = false;
+            this.btnReset.Location = new System.Drawing.Point(94, 249);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.Size = new System.Drawing.Size(75, 25);
             this.btnReset.TabIndex = 5;
             this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = true;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
+            // TreelistView
+            // 
+            this.TreelistView.Location = new System.Drawing.Point(238, 63);
+            this.TreelistView.Name = "TreelistView";
+            this.TreelistView.Size = new System.Drawing.Size(234, 180);
+            this.TreelistView.TabIndex = 6;
+            this.TreelistView.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 274);
+            this.ClientSize = new System.Drawing.Size(478, 291);
+            this.Controls.Add(this.TreelistView);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cbbTree);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.rtbClientlist);
             this.Name = "Form1";
             this.Text = "Tracker";
@@ -139,6 +148,7 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem preferenceToolStripMenuItem;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ListView TreelistView;
     }
 }
 
