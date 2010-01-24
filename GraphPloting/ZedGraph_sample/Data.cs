b67d9eeba79d0dc4443042a21055042f91f67ef1
edluke123 <edluke123@gmail.com>
2010-01-24 12:10:36 +0000

    None Complete graph Plotting

---
+++ GraphPloting/ZedGraph_sample/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Analysis
+{
+    public struct Data
+    {
+        public string IP;
+        public string Speed;
+        public string time;
+    }
+}
