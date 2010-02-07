0f7e5001c167c0ef1ecf93f62a4c1bb22016bc9a
eg_ckyai@stu.ust.hk <eg_ckyai@stu.ust.hk@33327d0a-b64f-11de-97ce-e783fd196cab>
2010-02-07 11:43:22 +0000

    graph generate move to dll

---
+++ trunk/cevfyp/Test/Test/Start.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Test
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.display = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(12, 33);
+            this.display.Name = "display";
+            this.display.ScrollGrace = 0;
+            this.display.ScrollMaxX = 0;
+            this.display.ScrollMaxY = 0;
+            this.display.ScrollMaxY2 = 0;
+            this.display.ScrollMinX = 0;
+            this.display.ScrollMinY = 0;
+            this.display.ScrollMinY2 = 0;
+            this.display.Size = new System.Drawing.Size(455, 293);
+            this.display.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 335);
+            this.Controls.Add(this.display);
+            this.Name = "Start";
+            this.Text = "Start";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl display;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
