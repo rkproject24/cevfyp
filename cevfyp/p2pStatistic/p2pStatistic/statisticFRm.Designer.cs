5d4b0ea5af484e02b810b89710cfdd02b8e19c16
eg_ckyai <eg_ckyai@stu.ust.hk>
2010-02-11 11:54:52 +0000

    statistic

---
+++ cevfyp/p2pStatistic/p2pStatistic/statisticFRm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace p2pStatistic
+{
+    partial class statisticFRm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphCon = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudPort = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zedGraphCon
+            // 
+            this.zedGraphCon.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphCon.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphCon.Name = "zedGraphCon";
+            this.zedGraphCon.ScrollGrace = 0;
+            this.zedGraphCon.ScrollMaxX = 0;
+            this.zedGraphCon.ScrollMaxY = 0;
+            this.zedGraphCon.ScrollMaxY2 = 0;
+            this.zedGraphCon.ScrollMinX = 0;
+            this.zedGraphCon.ScrollMinY = 0;
+            this.zedGraphCon.ScrollMinY2 = 0;
+            this.zedGraphCon.Size = new System.Drawing.Size(382, 256);
+            this.zedGraphCon.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Listen Port";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(151, 274);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(53, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudPort
+            // 
+            this.nudPort.Location = new System.Drawing.Point(76, 274);
+            this.nudPort.Maximum = new decimal(new int[] {
+            65532,
+            0,
+            0,
+            0});
+            this.nudPort.Name = "nudPort";
+            this.nudPort.Size = new System.Drawing.Size(69, 20);
+            this.nudPort.TabIndex = 4;
+            // 
+            // statisticFRm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 303);
+            this.Controls.Add(this.nudPort);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zedGraphCon);
+            this.Name = "statisticFRm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphCon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudPort;
+    }
+}
+
---
+++ cevfyp/p2pStatistic/p2pStatistic/statisticFRm.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+using Analysis;
+using ClassLibrary;
+
+namespace p2pStatistic
+{
+    public partial class statisticFRm : Form
+    {
+        plotgraph xmldata;
+        //int CurrentIndex = 0;
+        int statisticListen;
+        
+        IPAddress localAddr;
+        TcpListener statisticListener;
+        Thread listenerThread;
+
+        bool graphCreated;
+        private List<plotgraph> graphTreeData;
+        private int totalTree;
+        private bool[] xmlWriting;
+
+
+        public statisticFRm()
+        {
+            InitializeComponent();
+            //xmldata = new plotgraph(@"C:\Documents and Settings\eeuser\Desktop\cevfyp\cevfyp\Client\Client\bin\Debug\Tree0", false);
+            //xmldata = new plotgraph("Tree0", false);
+            graphCreated = false;
+            xmlWriting = null;
+            localAddr = IPAddress.Parse(TcpApps.LocalIPAddress());
+            totalTree = 0;
+            nudPort.Value = TcpApps.RanPort(1701, 1800);
+
+
+        }
+
+        public void listenCommand()
+        {
+            statisticListener = new TcpListener(localAddr, statisticListen);
+            statisticListener.Start();
+
+            while (true)
+            {
+                TcpClient client = statisticListener.AcceptTcpClient();
+                NetworkStream cstream = client.GetStream();
+                //TcpClient.Client.RemoteEndPoint.   
+
+                IPAddress clientendpt = ((IPEndPoint)client.Client.RemoteEndPoint).Address;
+                try
+                {
+                    //Get the Peer type- server/client
+                    byte[] responsePeerMsg = new byte[10];
+                    cstream.Read(responsePeerMsg, 0, responsePeerMsg.Length);
+
+                    string peertype = ByteArrayToString(responsePeerMsg);
+
+                    if (peertype.Contains("<newCurve>"))
+                    {
+                        byte[] treeMsg = new byte[4];
+                        cstream.Read(treeMsg, 0, treeMsg.Length);
+                        totalTree = BitConverter.ToInt32(treeMsg, 0);
+                        //responsePeerMsg = new byte[4];
+
+                        //cstream.Read(responsePeerMsg, 0, responsePeerMsg.Length);
+                        //int MsgSize = BitConverter.ToInt32(responsePeerMsg, 0);
+
+                        //byte[] responsePeerMsg2 = new byte[MsgSize];
+                        //cstream.Read(responsePeerMsg2, 0, responsePeerMsg2.Length);
+                        //string filePath = ByteArrayToString(responsePeerMsg2);
+
+                        //string[] messages = MsgContent.Split('@');
+
+                        //string filePath = messages[0];
+                        //xmldata = new plotgraph(filePath);
+
+                        xmldata = new plotgraph("");
+                        xmlWriting = new bool[totalTree];
+                        graphTreeData = new List<plotgraph>(totalTree);
+
+                        for (int i = 0; i < totalTree; i++)
+                        {
+                            graphTreeData.Add(new plotgraph("Tree" + i, true));
+                            xmlWriting[i] = false;
+                        }
+
+                        xmldata.CreateGraph(zedGraphCon, totalTree);
+                        graphCreated = true;
+                    }
+                    else if (peertype.Contains("<renewCur>"))
+                    {
+                        //replay whether graph is created
+                        byte[] createdbyte = BitConverter.GetBytes(graphCreated);
+                        cstream.Write(createdbyte, 0, createdbyte.Length);
+
+                        if (!graphCreated)//if graph is not created, skip below
+                            continue;
+
+                        byte[] treeMsg = new byte[4];
+                        cstream.Read(treeMsg, 0, treeMsg.Length);
+                        int tree = BitConverter.ToInt32(treeMsg, 0);
+
+                        //addRecord
+                        responsePeerMsg = new byte[4];
+                        cstream.Read(responsePeerMsg, 0, responsePeerMsg.Length);
+                        int MsgSize = BitConverter.ToInt32(responsePeerMsg, 0);
+
+                        byte[] responsePeerMsg2 = new byte[MsgSize];
+                        cstream.Read(responsePeerMsg2, 0, responsePeerMsg2.Length);
+                        string MsgContent = ByteArrayToString(responsePeerMsg2);
+                        string[] messages = MsgContent.Split('@');
+
+                        //DateTime start = DateTime.Parse(messages[0]);
+                        //DateTime end = DateTime.Parse(messages[1]);
+                        DateTime start = DateTime.FromBinary(long.Parse(messages[0]));
+                        DateTime end = DateTime.FromBinary(long.Parse(messages[1]));
+                        int size = Int32.Parse(messages[2]);
+
+                        if (xmlWriting[tree] == false)
+                        {
+                            xmlWriting[tree] = true;
+                            graphTreeData[tree].AddRecord(start, end, size);
+                            xmlWriting[tree] = false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.ToString());
+                    //Console.WriteLine(ex);
+                }
+                Thread.Sleep(50);
+            }
+            
+        }
+
+        private static string ByteArrayToString(byte[] bytes)
+        {
+            System.Text.Encoding enc = System.Text.Encoding.ASCII;
+            return enc.GetString(bytes);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < totalTree; i++)
+            {
+                if (xmlWriting == null)//stop loop if no client connected
+                    break;
+                if (xmlWriting[i] == false)
+                {
+                    xmlWriting[i] = true;
+                    xmldata.UpdateGraph(zedGraphCon, i);
+                    xmlWriting[i] = false;
+                }
+            }
+
+            this.zedGraphCon.AxisChange();
+            this.zedGraphCon.Refresh();
+
+            //CurrentIndex++;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            statisticListen = (Int32)nudPort.Value;
+
+            listenerThread = new Thread(new ThreadStart(listenCommand));
+            listenerThread.IsBackground = true;
+            listenerThread.Name = " listen_for_peers";
+            listenerThread.Start();
+        }
+    }
+}
